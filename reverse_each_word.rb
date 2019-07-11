5cefe00f86e387b73be39a97ef81c4eb18c4d762
Louis Lammerting <louis.lammerting@gmail.com>
2019-07-11 20:18:23 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,22 @@
+def reverse_each_word_with_each(string)
+  first_array = string.split(" ")
+  second_array = []
+  first_array.each do|string|
+    second_array << string.reverse
+  end
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  second_array = []
+  array.collect do|string|
+    second_array << string.reverse
+  end
+  test_array.join(" ")
+end
+
+
+reverse_each_word("Hello there, and how are you?")
+  #=> "olleH ,ereht dna woh era ?uoy"
+  
